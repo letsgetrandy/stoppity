8541ffeae8f9fc5a6ec0531cdb6d139c990e89ce
Randy Hunt <letsgetrandy@gmail.com>
2013-03-25 23:34:42 -0500

    manifest version 2, and aweber blocked

--- chrome/stoppity.js
+++ chrome/stoppity.js
@@ -4,54 +4,72 @@ var patterns = [
     'clickfuse.com'                 // clickfuse
 ];
 var scripts = window.document.getElementsByTagName('script');
-for (scriptfile in scripts) {
-    for (pattern in patterns) {
+for (var scriptfile in scripts) {
+    for (var pattern in patterns) {
         var rx = new RegExp(pattern);
-        if (rx.text(scriptfile.src))
+        if (rx.test(scriptfile.src))
             scriptfile.src = '';
     }
 }
 
 var classes = [
-	'popup-dom-lightbox-wrapper',	// popup domination
-	'jqmWindow'				        // jqm ads
+    'popup-dom-lightbox-wrapper',   // popup domination
+    'jqmWindow',                    // jqueryModal
+    'jqmOverlay'                    // jqueryModal Overlay
 ];
 var ids = [
-	'pty_pkg',				        // pippity poppity
-	'mod_wrapper',			        // modal wrapper
-	'AdSpotMovie',			        // AdSpot
-	'shortTail_D30_modal'	        // short tail
+    'pty_pkg',                      // pippity poppity
+    'mod_wrapper',                  // modal wrapper
+    'AdSpotMovie',                  // AdSpot
+    'shortTail_D30_modal',          // short tail
+    'cf_slideout_container'         //
 ];
 var childids = [
-	'rm_modal',				        // ringtone maker
+    'rm_modal'                      // ringtone maker
 ];
+//var cryptic_ids = [
+//    'popForm'
+//];
 
 window.setInterval (function () {
-
+    var i, el, id, cl, nodes;
     // check for the specified IDs and kill them
-	for (id=0; id<ids.length; id++) {
-		var el = window.document.getElementById(ids[id]);
-		if (el) {
-			el.parentNode.removeChild(el);
-			chrome.extension.sendRequest ({'type':ids[id]}, function(response){} );
-		}
-	}
+    for (id=0; id<ids.length; id++) {
+        el = window.document.getElementById(ids[id]);
+        if (el) {
+            el.parentNode.removeChild(el);
+            chrome.extension.sendRequest({'type':ids[id]});
+        }
+    }
     // kill anything containing the specified child ID
-	for (id=0; id<childids.length; id++) {
-		var el = window.document.getElementById(childids[id]);
-		if (el) {
-			el = el.parentNode;
-			el.parentNode.removeChild(el);
-			chrome.extension.sendRequest ({'type':childids[id]}, function(response){} );
-		}
-	}
+    for (id=0; id<childids.length; id++) {
+        el = window.document.getElementById(childids[id]);
+        if (el) {
+            el = el.parentNode;
+            el.parentNode.removeChild(el);
+            chrome.extension.sendRequest({'type':childids[id]});
+        }
+    }
     // kill anything with the specified CSS class
-	for (cl=0; cl<classes.length; cl++) {
-		var nodes = window.document.getElementsByClassName(classes[cl]);
-		for (i=0; i<nodes.length; i++) {
-			nodes[i].parentNode.removeChild(nodes[i]);
-			chrome.extension.sendRequest ({'type':classes[cl]}, function(response){} );
-		}
-	}
+    for (cl=0; cl<classes.length; cl++) {
+        nodes = window.document.getElementsByClassName(classes[cl]);
+        for (i=0; i<nodes.length; i++) {
+            nodes[i].parentNode.removeChild(nodes[i]);
+            chrome.extension.sendRequest({'type':classes[cl]});
+        }
+    }
+    //// kill elements even with cryptic IDs
+    //for (id=0; id<cryptic_ids.length; id++) {
+    //    nodes = document.querySelectorAll('[id^='+cryptic_ids[id]+']');
+    //    for (i=0; i<nodes.length; i++) {
+    //        nodes[i].parentNode.removeChild(nodes[i]);
+    //        chrome.extension.sendRequest({'type':cryptic_ids[id]});
+    //    }
+    //}
 
+    // aWeber
+    nodes = document.getElementsByClassName('af-form-close-button');
+    if (nodes.length) {
+        nodes[0].click();
+    }
 }, 500);
